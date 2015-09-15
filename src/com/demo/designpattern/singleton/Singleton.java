505f9a0eabd67576d26e1a2b0cb8af2fc8cb5161
aa-waikei-tam <waikei.tam@beyondsoft.com>
2015-09-15 18:09:17 +0800

    Update.

---
+++ src/com/demo/designpattern/singleton/Singleton.java
@@ -0,0 +1,15 @@
+package com.demo.designpattern.singleton;
+
+public class Singleton {
+
+	private static final Singleton singleton = new Singleton();
+
+	private Singleton() {
+
+	}
+
+	public static Singleton getInstance() {
+		return singleton;
+	}
+
+}
---
+++ src/com/demo/designpattern/singleton/SingletonTest.java
@@ -0,0 +1,13 @@
+package com.demo.designpattern.singleton;
+
+public class SingletonTest {
+
+	public static void main(String[] args) {
+//		Singleton singleton = new Singleton();
+		Singleton singleton = Singleton.getInstance();
+		Singleton1 singleton1 = Singleton1.getInstance();
+		Singleton2 singleton2 = Singleton2.getInstance();
+		Singleton3 singleton3 = Singleton3.getInstance();
+	}
+
+}
