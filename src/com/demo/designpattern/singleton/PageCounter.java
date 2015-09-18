f27b05b23d2cad63dc4ad6bcea3be96d48032ea8
aa-waikei-tam <waikei.tam@beyondsoft.com>
2015-09-18 17:29:41 +0800

    Update.

--- src/com/demo/designpattern/singleton/PageCounter.java
+++ src/com/demo/designpattern/singleton/PageCounter.java
@@ -1,12 +1,10 @@
 package com.demo.designpattern.singleton;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 public class PageCounter {
 
 	private static final PageCounter instance = new PageCounter();
 
-	private AtomicLong counter = new AtomicLong(0);
+	private long count = 0L;
 
 	private PageCounter() {
 
@@ -16,12 +14,12 @@ public class PageCounter {
 		return instance;
 	}
 
-	public long add() {
-		return counter.addAndGet(1);
+	public synchronized long add() {
+		return count = count + 1;
 	}
 
 	public long get() {
-		return counter.get();
+		return count;
 	}
 
 }
--- src/com/demo/designpattern/singleton/PageCounter1.java
+++ src/com/demo/designpattern/singleton/PageCounter1.java
@@ -1,8 +1,12 @@
 package com.demo.designpattern.singleton;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 public class PageCounter1 {
 
 	private static final PageCounter1 instance = new PageCounter1();
+
+	private AtomicLong counter = new AtomicLong(0);
 
 	private PageCounter1() {
 
@@ -10,6 +14,14 @@ public class PageCounter1 {
 
 	public static PageCounter1 getInstance() {
 		return instance;
+	}
+
+	public long add() {
+		return counter.addAndGet(1);
+	}
+
+	public long get() {
+		return counter.get();
 	}
 
 }
